ff727a0d904b192611e99baeff57ed168dee8f92
Siirl <111539223+Siirl@users.noreply.github.com>
2023-08-22 15:37:55 -0500

    Add files via upload

--- conexion.inc.php
+++ conexion.inc.php
@@ -4,6 +4,11 @@ $host="localhost";
 $user="root";
 $pass="";
 $dbname="distri";
+//conectarnos a la BD
+$link=mysqli_connect($host,$user,$pass) or die ("ERROR AL CONECTAR LA BD" .mysqli_error($link));
+//seleccionar la BD
+mysqli_select_db($link,"dbname") or die ("ERROR AL SELECCIONAR LA BD".mysqli_error($link));
+return $link
 
 }
 ?>
